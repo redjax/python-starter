052018e1733ee1828188b0c94d0fcd1f6feff025
redjax <none@none.com>
2023-12-30 01:29:58 -0500

    Add snoop

--- src/python_starter/__main__.py
+++ src/python_starter/__main__.py
@@ -4,8 +4,10 @@ from python_starter import say_hi
 from python_starter.core import AppSettings
 from python_starter.system_info import get_os_type, get_processor, py_ver
 from python_starter.utils.ex_utils import join_lists
+import snoop
 
-if __name__ == "__main__":
+@snoop(depth=2)
+def demo():
     print(say_hi())
 
     settings: AppSettings = AppSettings()
@@ -19,3 +21,6 @@ if __name__ == "__main__":
     print(
         f"Joined list: {join_lists(['test', 'test2', 'test3'], ['test4', 5, 'test6'])}"
     )
+    
+if __name__ == "__main__":
+    demo()
